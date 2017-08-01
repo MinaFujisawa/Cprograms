148429f7b9a58c486ac621b212bf98b66a2437ca
MinaFujisawa <fujimina37@gmail.com>
2017-08-01 11:04:24 -0700

    First C program MinMax

---
+++ MinMax.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+	printf("The MIN value you can store in (INT) variable is %d\n", INT_MIN)
+	printf("Hello world\n");
+	return 0;
+}
+
