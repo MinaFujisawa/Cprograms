38109f6e356c9dcd62a586375b19691fe424b07f
MinaFujisawa <fujimina37@gmail.com>
2017-08-01 11:49:35 -0700

    fix

--- MinMax.c
+++ MinMax.c
@@ -3,7 +3,7 @@
 #include <float.h>
 
 int main() {
-	printf("The MIN value you can store in (INT) variable is %d\n", INT_MIN)
+	printf("The MIN value you can store in (INT) variable is %d\n", INT_MIN);
 	printf("Hello world\n");
 	return 0;
 }
