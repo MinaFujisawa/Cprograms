aaea205d0a6a10d8a09a73384c7df5d57052e58b
MinaFujisawa <fujimina37@gmail.com>
2017-08-03 12:17:21 -0700

    add lab2

---
+++ lab2.c
@@ -0,0 +1,21 @@
+//1 The extra step we take is adding 1 to our complemented(flipping bits) number.
+//2-1. 10111
+//2-2. 10101
+//2-3. 11001
+//2-4. 11111
+//3.
+//4.  In 2’s complement we have only one way to represent 0
+//5 we need to convert decimal 7 to binary
+//6 we need to convert decimal 7 to binary and do one’s complement, then plus 1 to the binary
+//7. If the decimal is positive number, you just need to convert it to binary.if it’s negative number, follow the same flow as the above and then plus 1 from the binary number.
+//8-1. 110000
+//8-2. 001101
+//8-3. 111101
+//8-4. 110110
+//8-5. 011010
+//8-6. 100001
+
+#include <stdio.h>
+int main(){
+	return 0;
+}
